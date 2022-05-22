adf20db85e1b74d82208b412ee0615a37952fd26
BWLKyh <1579121394@qq.com>
2022-05-22 23:27:27 +0800

    finished

---
+++ part3/fed-e-task-03-04/code/blog-strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3422e467ec22f72202669a7ecced0bd2'),
+    },
+  },
+});
---
+++ part3/fed-e-task-03-04/code/blog-strapi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '504a1d48-9315-4410-8632-f75dab3a5a33'
+};
